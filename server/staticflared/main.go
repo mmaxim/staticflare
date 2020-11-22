247c3e4afb5f2e40acef55b1384f681e753eb4b5
Mike Maxim <mike@keyba.se>
2020-11-21 22:50:37 -0500

    opts

--- server/staticflared/main.go
+++ server/staticflared/main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"mmaxim.org/staticflare/server"
 )
 
+type Options struct {
+	Addr string
+}
+
+func (o Options) check() {
+	if len(o.Addr) == 0 {
+		usage()
+	}
+}
+
+func usage() {
+	fmt.Printf("usage: staticflared <--addr addr>\n")
+	os.Exit(3)
+}
+
+func config() (opts Options) {
+	flag.StringVar(&opts.Addr, "addr", os.Getenv("STATICFLARED_ADDR"), "listen addr for the server")
+	flag.Parse()
+	opts.check()
+	return opts
+}
+
 func main() {
-	if err := server.NewServer("localhost:8080").Run(); err != nil {
+	opts := config()
+	if err := server.NewServer(opts.Addr).Run(); err != nil {
 		log.Printf("error running server: %s\n", err)
 	}
 }
